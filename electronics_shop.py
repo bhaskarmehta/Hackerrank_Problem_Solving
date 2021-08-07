f85dbc48b254c8830a6268f203d07c2bea1350eb
Bhaskar mehta <bhaskarmehta422@gmail.com>
2021-08-07 13:43:45 +0530

    added print to electronics

--- electronics_shop.py
+++ electronics_shop.py
@@ -1,2 +1,17 @@
-#comment for testting
-print("hello")
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    l = []
+    for i in range(0, len(keyboards)):
+        for j in range(0, len(drives)):
+
+            if (keyboards[i] + drives[j]) <= b:
+                l.append(keyboards[i] + drives[j])
+    if len(l) == 0:
+        return -1
+    else:
+        return max(l)
+key=[3,1]
+drives=[5,2,8]
+print(getMoneySpent(key,drives,10))
